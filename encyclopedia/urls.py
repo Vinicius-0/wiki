c1420c1fe1e6142023d0e0cca7134eb35e1e0411
Vinícius de Sousa Brito <viniciusvsb0@gmail.com>
2021-11-12 17:55:40 -0300

    Add 'Search' and 'Random Page' requirements

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:wikiPage>", views.item, name="item"),
+    path("search", views.search, name="search"),
+    path("random", views.random, name="random"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 
 from . import util
 
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 import markdown2
+
+import secrets
 
 
 def index(request):
@@ -23,3 +28,26 @@ def item(request, wikiPage):
             "wikiPage": wikiPage,
             "content": markdown2.markdown(util.get_entry(wikiPage))
         })
+
+
+def search(request):
+    value = request.GET.get('q', '')
+    entry = util.get_entry(value)
+    if entry != None:
+        return HttpResponseRedirect(reverse("item", kwargs={"wikiPage": value}))
+    else:
+        entries = []
+        for entry in util.list_entries():
+            if value.lower() in entry.lower():
+                entries.append(entry)
+
+        return render(request, "search/index.html", {
+            "entries": entries,
+            "value": value
+        })
+
+
+def random(request):
+    entries = util.list_entries()
+    randomEntry = secrets.choice(entries)
+    return HttpResponseRedirect(reverse("item", kwargs={"wikiPage": randomEntry}))
